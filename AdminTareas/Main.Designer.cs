454fab402eec45e0a74a2a61ece988981321ff7b
Soarsa89 <soarsa89@gmail.com>
2025-09-02 20:43:39 -0600

    Incio

---
+++ AdminTareas/Main.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AdminTareas
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            fluentDesignFormContainer1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer();
+            accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
+            accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            fluentDesignFormControl1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl();
+            fluentFormDefaultManager1 = new DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager(components);
+            barEditItem1 = new DevExpress.XtraBars.BarEditItem();
+            repositoryItemFontEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemFontEdit();
+            accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement3 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            ((System.ComponentModel.ISupportInitialize)accordionControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fluentFormDefaultManager1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)repositoryItemFontEdit1).BeginInit();
+            SuspendLayout();
+            // 
+            // fluentDesignFormContainer1
+            // 
+            fluentDesignFormContainer1.Dock = DockStyle.Fill;
+            fluentDesignFormContainer1.Location = new Point(260, 31);
+            fluentDesignFormContainer1.Name = "fluentDesignFormContainer1";
+            fluentDesignFormContainer1.Size = new Size(431, 442);
+            fluentDesignFormContainer1.TabIndex = 0;
+            fluentDesignFormContainer1.Click += fluentDesignFormContainer1_Click;
+            // 
+            // accordionControl1
+            // 
+            accordionControl1.Dock = DockStyle.Left;
+            accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement1 });
+            accordionControl1.Location = new Point(0, 31);
+            accordionControl1.Name = "accordionControl1";
+            accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
+            accordionControl1.Size = new Size(260, 442);
+            accordionControl1.TabIndex = 1;
+            accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
+            // 
+            // accordionControlElement1
+            // 
+            accordionControlElement1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement2, accordionControlElement3 });
+            accordionControlElement1.Name = "accordionControlElement1";
+            accordionControlElement1.Text = "Element1";
+            // 
+            // fluentDesignFormControl1
+            // 
+            fluentDesignFormControl1.FluentDesignForm = this;
+            fluentDesignFormControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { barEditItem1 });
+            fluentDesignFormControl1.Location = new Point(0, 0);
+            fluentDesignFormControl1.Manager = fluentFormDefaultManager1;
+            fluentDesignFormControl1.Name = "fluentDesignFormControl1";
+            fluentDesignFormControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] { repositoryItemFontEdit1 });
+            fluentDesignFormControl1.Size = new Size(691, 31);
+            fluentDesignFormControl1.TabIndex = 2;
+            fluentDesignFormControl1.TabStop = false;
+            fluentDesignFormControl1.TitleItemLinks.Add(barEditItem1);
+            // 
+            // fluentFormDefaultManager1
+            // 
+            fluentFormDefaultManager1.Form = this;
+            fluentFormDefaultManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { barEditItem1 });
+            fluentFormDefaultManager1.MaxItemId = 1;
+            fluentFormDefaultManager1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] { repositoryItemFontEdit1 });
+            // 
+            // barEditItem1
+            // 
+            barEditItem1.Caption = "barEditItem1";
+            barEditItem1.Edit = repositoryItemFontEdit1;
+            barEditItem1.Id = 0;
+            barEditItem1.Name = "barEditItem1";
+            // 
+            // repositoryItemFontEdit1
+            // 
+            repositoryItemFontEdit1.AutoHeight = false;
+            repositoryItemFontEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] { new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo) });
+            repositoryItemFontEdit1.Name = "repositoryItemFontEdit1";
+            // 
+            // accordionControlElement2
+            // 
+            accordionControlElement2.Name = "accordionControlElement2";
+            accordionControlElement2.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement2.Text = "Element2";
+            // 
+            // accordionControlElement3
+            // 
+            accordionControlElement3.Name = "accordionControlElement3";
+            accordionControlElement3.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement3.Text = "Element3";
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(691, 473);
+            ControlContainer = fluentDesignFormContainer1;
+            Controls.Add(fluentDesignFormContainer1);
+            Controls.Add(accordionControl1);
+            Controls.Add(fluentDesignFormControl1);
+            FluentDesignFormControl = fluentDesignFormControl1;
+            Name = "Main";
+            NavigationControl = accordionControl1;
+            Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)accordionControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fluentFormDefaultManager1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)repositoryItemFontEdit1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer fluentDesignFormContainer1;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl fluentDesignFormControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager fluentFormDefaultManager1;
+        private DevExpress.XtraBars.BarEditItem barEditItem1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemFontEdit repositoryItemFontEdit1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement3;
+    }
+}
