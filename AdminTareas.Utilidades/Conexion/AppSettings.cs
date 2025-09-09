bac7daa9e8f205067e6193bd4ad5d1cc757a4864
Soarsa89 <soarsa89@gmail.com>
2025-09-08 21:40:30 -0600

    Fix DB Directorio

--- AdminTareas.Utilidades/Conexion/AppSettings.cs
+++ AdminTareas.Utilidades/Conexion/AppSettings.cs
@@ -1,4 +1,25 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿//using Microsoft.Extensions.Configuration;
+
+//namespace AdminTareas.Utilidades.Conexion
+//{
+//    public static class AppSettings
+//    {
+//        private static IConfigurationRoot _config;
+
+//        static AppSettings()
+//        {
+//            _config = new ConfigurationBuilder()
+//                .SetBasePath(Directory.GetCurrentDirectory())
+//                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+//                .Build();
+//        }
+
+//        public static string ConnectionString =>
+//            _config.GetConnectionString("DefaultConnection")!;
+//    }
+//}
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace AdminTareas.Utilidades.Conexion
 {
@@ -10,11 +31,20 @@ namespace AdminTareas.Utilidades.Conexion
         {
             _config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .Build();
         }
 
-        public static string ConnectionString =>
-            _config.GetConnectionString("DefaultConnection")!;
+        public static string ConnectionString
+        {
+            get
+            {
+                
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;                
+                var dbPath = Path.Combine(basePath, "adminTareas.db");                
+                var defaultConnection = _config.GetConnectionString("DefaultConnection");                
+                return File.Exists(dbPath) ? $"Data Source={dbPath}" : defaultConnection!;
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Reto técnico - Administador de tareas
 
 Es una aplicación de escritorio para administrar tareas sencillas, con una interfaz de usuario optimizada y validación de datos.
+
+## Características principales
+
+- Crear, editar y eliminar tareas.
+- Definir estado de la tarea (Pendiente, En Proceso, Completada).
+- Persistencia de datos con Entity Framework Core y SQLite.
+- Patrón MVVM aplicado en Windows Forms con .NET 8.
+- Validaciones en formularios para garantizar integridad de datos.
+- Interfaz construida con DevExpress para una mejor experiencia de usuario.
+
+## Entorno de Desarrollo
+
+- SDK .Net8
+- Visual Studio 2022
+- DevExpress
+- EtintyFramework
+- SQlite
+- CommunityToolkit.Mvvm
+
+## Ejecutar El proyecto
+
+- Clonar el repositorio : '<https://github.com/Soarsa89/RetoTecnicoNCQ.git>'
